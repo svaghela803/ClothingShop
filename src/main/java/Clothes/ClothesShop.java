b8e2f2a411be2195750f1fdecb91469f1685d982
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-08-22 15:08:39 +0100

    Created clothesshop class and getters and setters

---
+++ src/main/java/Clothes/ClothesShop.java
@@ -0,0 +1,30 @@
+package Clothes;
+
+public class ClothesShop {
+
+    private String name;
+    private String season;
+
+//    constructor
+    public ClothesShop(String name){
+        this.name = name;
+        this.season = season;
+    }
+
+//    getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSeason(){
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+}
