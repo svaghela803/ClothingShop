7010b5c102aed9738adb9be753d0c4293f759359
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-08-23 16:21:37 +0100

    getters and setters for jacktes

--- src/main/java/Clothes/Jacket.java
+++ src/main/java/Clothes/Jacket.java
@@ -9,6 +9,30 @@ public class Jacket extends ClothingItem {
     boolean hood;
     String fastening;
 
+//constructor
 
+    public Jacket(boolean hood, String fastening) {
+        super(fastening);
+        this.hood = hood;
+        this.fastening = fastening;
+    }
 
-}
+//    getters and setters
+
+    public boolean gethood() {
+        return hood;
+    }
+    public void sethood (boolean hood) {
+        this.hood = hood;
+    }
+
+    public String getFastening() {
+        return fastening;
+    }
+
+    public void setFastening (String fastening) {
+        this.fastening = fastening;
+    }
+
+    }
+
