23d93c9dbb4d9f8d51b70ca3db226a971afcaf98
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-08-23 16:28:41 +0100

    getters and setters trouser

--- src/main/java/Clothes/Trouser.java
+++ src/main/java/Clothes/Trouser.java
@@ -1,8 +1,26 @@
 package Clothes;
 
-public class Trouser {
+public class Trouser extends ClothingItem {
 
     private String fastening;
     private double legLength;
+
+//constructor
+    public Trouser(String name) {
+        super(name);
+        this.legLength = legLength;
+    }
+
+//    getters and setters
+
+    public double getLegLength() {
+        return legLength;
+    }
+
+    public void setColour (double legLength) {
+        this.legLength = legLength;
+    }
+
+//    method
 
 }
